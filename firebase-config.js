84cc72cd2c1d79d0127756135e6c2beff1b21e8c
mhamadnerway932-oss <mhamadnerway932@gmail.com>
2026-09-13 23:39:58 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// 🔐 Firebase Configuration
+window.firebaseConfig = {
+    apiKey: atob("QUl6YVN5QlhTREVqbnRsb1p3MXl0dG5EVmJoUXlmRzRoeVpzaGpr"),
+    authDomain: atob("Y2xlYXJhbmNlcG9ydGFsLTEyOGY5LmZpcmViYXNlYXBwLmNvbQ=="),
+    projectId: atob("Y2xlYXJhbmNlcG9ydGFsLTEyOGY5"),
+    storageBucket: atob("Y2xlYXJhbmNlcG9ydGFsLTEyOGY5LmZpcmViYXNldG9yYWdlLmFwcA=="),
+    messagingSenderId: atob("ODA0MDY2MDQ4ODYw"),
+    appId: atob("MTo4MDQwNjYwNDg4NjA6d2ViOmE1ZDk1Y2NmMjY2YjEyN2Q1ODlmOTk="),
+    measurementId: atob("Ry0yWjNRQkdHN0ZF")
+};
